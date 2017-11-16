2fc5442f0067e08c43a50cc003dc7cacd4097ad8
Alberto L <alberto1llv@gmail.com>
2017-11-16 21:53:00 +0100

    Added Physics and Math class.

---
+++ Cut the Rope/Assets/Scripts/Math.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom
+{
+    public static class Math
+    {
+        /// <summary>
+        /// Calculates a matrix-vector multiplication.
+        /// Note: Only Vector3/3x3 Matrix with 3x3 Matrix [Vector3 array with 3 elements, each Vector3 represents a column] supported.
+        /// </summary>
+        public static Vector3 VectorMatrixProduct(Vector3 vector, Vector3[] matrix)
+        {
+            if (matrix.Length != 3)
+                return Vector3.zero;
+
+            Vector3 result = Vector3.zero;
+
+            result += new Vector3(      matrix[0].x * vector.x + matrix[1].x * vector.y + matrix[2].x * vector.z, 0, 0);
+            result += new Vector3(0,    matrix[0].y * vector.x + matrix[1].y * vector.y + matrix[2].y * vector.z, 0);
+            result += new Vector3(0, 0, matrix[0].z * vector.x + matrix[1].z * vector.y + matrix[2].z * vector.z);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates a matrix-matrix multiplication.
+        /// Note: Only Vector3/3x3 Matrix with 3x3 Matrix [Vector3 array with 3 elements, each Vector3 represents a column] supported.
+        /// </summary>
+        public static Vector3[] MatrixMatrixProduct(Vector3[] matrixA, Vector3[] matrixB)
+        {
+            if (matrixA.Length != 3 || matrixB.Length != 3)
+            {
+                return new Vector3[3];
+                Debug.LogError("Error!");
+            }
+
+            Vector3[] result = new Vector3[3];
+
+            for (int a = 0; a < 3; a++)
+            {
+                result[a] = new Vector3(        matrixA[0].x * matrixB[a].x + matrixA[1].x * matrixB[a].y + matrixA[2].x * matrixB[a].z, 0, 0);
+                result[a] += new Vector3(0,     matrixA[0].y * matrixB[a].x + matrixA[1].y * matrixB[a].y + matrixA[2].y * matrixB[a].z, 0);
+                result[a] += new Vector3(0, 0,  matrixA[0].z * matrixB[a].x + matrixA[1].z * matrixB[a].y + matrixA[2].z * matrixB[a].z);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Transposes a matrix. Only 3x3 Matrix supported [Vector3 array with 3 elements].
+        /// </summary>
+        public static Vector3[] TransposeMatrix(Vector3[] matrix)
+        {
+            Vector3[] result = new Vector3[3];
+
+            if (matrix.Length != 3)
+                return result;
+
+            result[0] = new Vector3(matrix[0].x, matrix[1].x, matrix[2].x);
+            result[1] = new Vector3(matrix[0].y, matrix[1].y, matrix[2].y);
+            result[2] = new Vector3(matrix[0].z, matrix[1].z, matrix[2].z);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the distance from a point to an axis.
+        /// </summary>
+        public static float GetOrthoToAxis(Vector3 origin, Vector3 n, Vector3 point)
+        {
+            Vector3 direction = n;
+            Vector3 startingPoint = origin;
+
+            Ray ray = new Ray(startingPoint, direction);
+            float distance = Vector3.Cross(ray.direction, point - ray.origin).magnitude;
+
+            return distance;
+        }
+
+        /// <summary>
+        /// Converts a quaternion to its equivalent rotationmatrix.
+        /// </summary>
+        public static Vector3[] GetRotationMatrix(Quaternion q)
+        {
+            float a = q.x;
+            float b = q.y;
+            float c = q.z;
+            float d = q.w;
+
+            Vector3 firstColumn     = new Vector4(1f - 2f * (c * c + d * d), 2f * (b * c + a * d), 2f * (b * d - a * c)/*, 0*/);
+            Vector3 secondColumn    = new Vector4(2f * (b * c - a * d), 1f - 2f * (d * d + b * b), 2f * (c * d + a * b)/*, 0*/);
+            Vector3 thirdColumn     = new Vector4(2f * (b * d + a * c), 2f * (c * d - a * b), 1f - 2f * (b * b + c * c)/*, 0*/);
+            //Vector3 fourthColumn    = new Vector4(0, 0, 0, 1);
+
+            return new Vector3[3] { firstColumn, secondColumn, thirdColumn };
+        }
+
+        public static float GetVolumenOfSphere(float radius)
+        {
+            return Mathf.PI * (Mathf.Pow(radius * 2f, 3f) / 6f);
+        }
+
+        public static float GetRadiusOfSphere(float volumen)
+        {
+            return Mathf.Pow(volumen / (4f / 3f * Mathf.PI), 1f / 3f);
+        }
+
+        public static Vector3[] GetTensorOfCylinder(float radius, float height, float Mass)
+        {
+            Vector3[] result = new Vector3[3];
+
+            float a = 1f / 2f * (Mathf.Pow(radius, 2) + Mathf.Pow(height, 2) / 3f);
+
+            result[0] = new Vector3(a, 0, 0) * Mass / 2f;
+            result[1] = new Vector3(0, Mathf.Pow(radius, 2), 0) * Mass / 2f;
+            result[2] = new Vector3(0, 0, a) * Mass / 2f;
+
+            return result;
+        }
+
+        public static Vector3[] GetTensorOfSphere(float radius, float Mass)
+        {
+            Vector3[] result = new Vector3[3];
+
+            float a = 2f / 5f * Mass * Mathf.Pow(radius, 2f);
+            Debug.Log("a is " + a);
+
+            result[0] = new Vector3(a, 0, 0);
+            result[1] = new Vector3(0, a, 0);
+            result[2] = new Vector3(0, 0, a);
+
+            return result;
+        }
+
+        public static Vector3[] GetTensorOfCube(float size, float Mass)
+        {
+            Vector3[] result = new Vector3[3];
+
+            float a = 1f / 6f * Mass * Mathf.Pow(size, 2f);
+            Debug.Log("a is " + a);
+
+            result[0] = new Vector3(a, 0, 0);
+            result[1] = new Vector3(0, a, 0);
+            result[2] = new Vector3(0, 0, a);
+
+            return result;
+        }
+
+        public static float GetRadian(float degree)
+        {
+            return (degree / 360f) * 2f * Mathf.PI;
+        }
+    }
+}
---
+++ Cut the Rope/Assets/Scripts/Physics.cs
@@ -0,0 +1,309 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom
+{
+    public static class Physics
+    {
+        public struct Pulse
+        {
+            public Vector3 Direction;
+            public float Energy;
+        }
+
+        public static List<Vector3> n;
+        public static List<Vector3> j;
+        public static List<float> j_skalar;
+        public static List<Vector3> contact;
+
+        /// <summary>
+        /// Gets the gravitational force between two MassObjects.
+        /// </summary>
+        /// <param name="a">The first MassObject.</param>
+        /// <param name="b">The second MassObject.</param>
+        /// <returns>The gravitational force.</returns>
+        public static float GetGravitationalForce(MassObject a, MassObject b, float gravitationalConstant)
+        {
+            return gravitationalConstant * ((float)a.Mass * (float)b.Mass) / Mathf.Pow(Vector3.Distance(a.transform.position, b.transform.position), 2);
+        }
+
+        /// <summary>
+        /// Gets the gravitational force between two MassObjects.
+        /// </summary>
+        /// <param name="a">The first MassObject.</param>
+        /// <param name="b">The second MassObject.</param>
+        /// <returns>The gravitational force.</returns>
+        public static float GetGravitationalForce(float massA, double massB, float Distance, float gravitationalConstant)
+        {
+            return gravitationalConstant * (float)((massA * massB) / Mathf.Pow(Distance, 2));
+        }
+
+        /// <summary>
+        /// Calculates the outcome of a Collision between two MassObjects.
+        /// </summary>
+        /// <param name="massObject1">The first MassObject.</param>
+        /// <param name="massObject2">The second MassObject.</param>
+        /// <returns>The rotational pulse.</returns>
+        public static Pulse CalculatePulse(MassObject massObject1, MassObject massObject2, ContactPoint contactPoint)
+        {
+            Pulse result = new Pulse();
+
+            float m = (float)massObject1.Mass;
+
+            // _________________________________________
+            //
+            // -----------------------------------------
+            // Errechnung der Rotationsmatrix.
+            // -----------------------------------------
+            // _________________________________________
+
+            Vector3[] M;        // Orientierung des Körpers.
+
+            Vector3[] M_inverse;
+
+            M = Math.GetRotationMatrix(massObject1.transform.rotation);
+
+            M_inverse = Math.TransposeMatrix(M);
+
+            // _________________________________________
+            //
+            // -----------------------------------------
+            // Errechnung des Trägheitstensors.
+            // -----------------------------------------
+            // _________________________________________
+
+            Vector3[] I;
+
+            I = Math.GetTensorOfSphere(massObject1.Radius, (float)massObject1.Mass);
+
+            Vector4 column0 = new Vector4(I[0].x, I[0].y, I[0].z, 0);
+            Vector4 column1 = new Vector4(I[1].x, I[1].y, I[1].z, 0);
+            Vector4 column2 = new Vector4(I[2].x, I[2].y, I[2].z, 0);
+            Vector4 column3 = new Vector4(0, 0, 0, 1);
+
+            Matrix4x4 I_4 = new Matrix4x4(column0, column1, column2, column3);
+            Matrix4x4 I_4_Inverse = Matrix4x4.Inverse(I_4);
+
+            Vector3 column03 = new Vector3(I_4_Inverse.m00, 0, 0);
+            Vector3 column13 = new Vector3(0, I_4_Inverse.m11, 0);
+            Vector3 column23 = new Vector3(0, 0, I_4_Inverse.m22);
+
+            Vector3[] I_inverse = new Vector3[3] { column03, column13, column23 };
+
+
+            // _________________________________________
+            //
+            // -----------------------------------------
+            // Erstellung des Kontaktkoordinatensystems.
+            // -----------------------------------------
+            // _________________________________________
+
+            Vector3 n = contactPoint.normal; // Kontaktnormale.
+
+            Vector3 xAchse = n;
+            Vector3 errateneAchse;
+            Vector3 zAchse;
+            Vector3 yAchse;
+
+            do
+            {
+                errateneAchse = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                zAchse = Vector3.Cross(xAchse, errateneAchse);
+                yAchse = Vector3.Cross(xAchse, zAchse);
+            }
+            while (yAchse == Vector3.zero || zAchse == Vector3.zero);
+
+            // _________________________________________
+            //
+            // -----------------------------------------
+            // Erstellung der Basismatrix.
+            // -----------------------------------------
+            // _________________________________________
+
+            Vector3[] M_basis = new Vector3[3];
+            M_basis[0] = xAchse;
+            M_basis[1] = yAchse.normalized;
+            M_basis[2] = zAchse.normalized;
+
+            Vector3[] M_basis_inverse = Math.TransposeMatrix(M_basis);
+
+            // _________________________________________
+            //
+            // -----------------------------------------
+            // Errechnung des impulsiven Drehmoments.
+            // -----------------------------------------
+            // _________________________________________
+
+            Vector3 qRel;   // Relativer Kontaktpunkt.
+            Vector3 q;      // Ortsvektor des Kontaktpunktes.
+            Vector3 p;      // Ortsvektor des Objektschwerpunktes.
+
+            q = Math.VectorMatrixProduct(contactPoint.point, M_basis_inverse);
+            p = Math.VectorMatrixProduct(massObject1.transform.position, M_basis_inverse);
+
+            qRel = q - p;
+
+            // -----------------------------------------
+
+            Vector3 u;      // Rotationsimpuls.
+            Vector3 j;      // Wirkender linearer Impuls. [Nicht zu verwechseln mit J (Gesamtimpuls!)]
+
+            j = massObject2.Energy * massObject2.Direction - massObject1.Energy * massObject1.Direction;
+            u = Vector3.Cross(qRel, j);
+
+            // -----------------------------------------
+
+            Vector3 u_contact;  // Rotationsimpuls in Richtung der Kontaktnormalen.
+            float j_skalar;     // Wirkender linearer Impuls in Richtung der Kontaktnormalen.
+
+            float c = j.magnitude;
+            float b = Math.GetOrthoToAxis(Vector3.zero, n, j);
+
+            j_skalar = Mathf.Sqrt(Mathf.Pow(c,2) - Mathf.Pow(b,2));
+            u_contact = Vector3.Cross(qRel, j_skalar * n);
+
+            // -----------------------------------------
+
+            Vector3 O;      // Rotationsgeschwindigkeit, O = M * I^-1 * M^-1 * u_contact.
+
+            Vector3[] I_inverse_Basiswechsel = Math.MatrixMatrixProduct(Math.MatrixMatrixProduct(I_inverse, M_basis_inverse), M_basis);
+
+            O = Math.VectorMatrixProduct(u_contact, I_inverse_Basiswechsel);
+
+
+            // -----------------------------------------
+
+            Vector3 v_rot;  // lineare Rotationsgeschwindigkeit.
+
+            v_rot = Vector3.Cross(O, qRel);
+
+            Vector3 rotationsAchse = Vector3.Cross(contactPoint.point, contactPoint.point + j_skalar * n).normalized;
+
+            // -----------------------------------------
+
+            float delta_v; // Geschwindigkeitsänderung in Richtung der Kontaktnormalen. Δv = j * 1 / m + n ((I^−1 (qrel × jn)) × qrel)
+
+            delta_v = j_skalar * 1 / m + Vector3.Dot(n, v_rot);
+
+            // -----------------------------------------
+
+            float J;      // Gesamtimpuls. [Nicht zu verwechseln mit j (Wirkender linearer Impuls!)]
+
+            J = delta_v / (1 / m + Vector3.Dot(n, Vector3.Cross(Math.VectorMatrixProduct(Vector3.Cross(qRel, n), I_inverse), qRel)));
+
+            Debug.Log("j: " + J);
+
+            // _________________________________________
+            //
+            // -----------------------------------------
+            // Errechnung der Separierungsgeschwindigkeit vs.
+            // -----------------------------------------
+            // _________________________________________
+
+            Vector3 qo;     // Lineare Geschwindigkeit durch Rotation (ohne linearen Anteil).
+
+            qo = Vector3.Cross(O, qRel);
+
+            // -----------------------------------------
+
+            Vector3 vr;     // Gesamtgeschwindigkeit.
+
+            vr = qo + massObject1.RotationVector;
+
+            // -----------------------------------------
+
+            Vector3 v;      // Gesamtgeschwindigkeit in Kontaktkoordinaten.
+
+            v = Math.VectorMatrixProduct(vr, M_basis_inverse);
+
+            // -----------------------------------------
+
+            float vs;       // Separierungsgeschwindigkeit.
+
+            vs = vr.x;
+
+            // -----------------------------------------
+
+            float delta_vs;
+
+            //delta_vs = -(1 + massObject1.C) * vs;
+
+            // -----------------------------------------
+
+
+            //J = delta_vs / (1 / m + Vector3.Dot(Vector3.Cross(Math.VectorMatrixProduct(Math.VectorMatrixProduct(Math.VectorMatrixProduct(Vector3.Cross(qRel, n), M_basis_inverse), I_inverse), M_basis), qRel), n));
+
+            // -----------------------------------------
+
+            Vector3 g_contact;  // Gesamtimpuls in Kontaktkoordinaten.
+
+            g_contact = new Vector3(J, 0, 0);
+
+            // -----------------------------------------
+
+            Vector3 g_world;    // Gesamtimpuls in Weltkoordinaten.
+
+            g_world = Math.VectorMatrixProduct(g_contact, M_basis);
+
+            // -----------------------------------------
+
+            Vector3 delta_p;    // Änderung der linearen Geschwindigkeit.
+
+            delta_p = new Vector3(g_world.x / m, g_world.y / m, g_world.z / m);
+
+            // -----------------------------------------
+
+            Vector3 delta_0;    // Änderung der Rotationsgeschwindigkeit.
+
+            delta_0 = Math.VectorMatrixProduct(Math.VectorMatrixProduct(Math.VectorMatrixProduct(Vector3.Cross(qRel, g_world), M_inverse), I_inverse), M);
+
+            // -----------------------------------------
+
+            //Debug.Log("deltaV: " + delta_v);
+            //Debug.Log("u_contact: " + u_contact);
+            //Debug.Log("O: " + O);
+            //Debug.Log("qRel: " + qRel);
+            //Debug.Log("qo: " + qo);
+            //Debug.Log("vr: " + vr);
+            //Debug.Log("v: " + v);
+            //Debug.Log("vs: " + vs);
+            //Debug.Log("delta_vs: " + delta_vs);
+            //Debug.Log("J: " + J);
+            //Debug.Log(delta_0);
+
+            result.Direction = n;
+            result.Energy = J;
+
+            Physics.j.Add(j);
+            Physics.j_skalar.Add(j_skalar);
+            Physics.n.Add(n);
+
+            return result;
+        }
+
+        public static float GetEnergy(float speed, float mass)
+        {
+            return 0.5f * mass * Mathf.Pow(speed, 2);
+        }
+
+        public static float GetSpeed(float energy, float mass)
+        {
+            return Mathf.Sqrt(energy / (0.5f * mass));
+        }
+
+        //protected void ConvertSpeedToEnergy()
+        //{
+        //    Vector3 movement = Speed * Direction * Mass;
+
+        //    Direction = movement.normalized;
+
+        //    float divider = Mathf.Abs(Direction.x) + Mathf.Abs(Direction.y) + Mathf.Abs(Direction.z);
+
+        //    if (divider != 0)
+        //        Energy = (Mathf.Abs(movement.x) + Mathf.Abs(movement.y) + Mathf.Abs(movement.z)) / divider;
+        //    else
+        //        Energy = 0;
+        //}
+    }
+}
